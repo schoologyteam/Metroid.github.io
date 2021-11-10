9b382e7e5db6e94e8ac8c902377aea3384fee176
LandonGithub07 <93219767+LandonGithub07@users.noreply.github.com>
2021-11-10 12:23:12 -0600

    Update IodineGBACoreGlueCode.js

--- gba/user_scripts/IodineGBACoreGlueCode.js
+++ gba/user_scripts/IodineGBACoreGlueCode.js
@@ -88,6 +88,7 @@ var games = {
     "gba_video_pokemon_2":"Pokemon Video Pak 2",
     "gba_video_pokemon_3":"Pokemon Video Pak 3",
     "gba_video_pokemon_4":"Pokemon Video Pak 4",
+    "scoobydoo":"Scooby Doo Mystery Mayhem",
     "sonic_advance":"Sonic Advance",
     "sonic_advance2":"Sonic Advance 2",
     "sonic_advance3":"Sonic Advance 3",
